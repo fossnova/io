550efc448aaeaa01d2b05a5a5741b5cc4a76151a
Richard Opalka <opalka.richard@gmail.com>
2012-05-30 16:06:56 +0200

    introduce BrokenInputStream, BrokenOutputStream, BrokenReader & BrokenWriter

---
+++ src/main/java/org/fossnova/io/BrokenReader.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2012, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.fossnova.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.CharBuffer;
+
+/**
+ * A <code>BrokenReader</code> always throws <code>IOException</code>.
+ * <p>
+ * This class is thread safe. 
+ * </p>
+ * 
+ * @author <a href="mailto:opalka dot richard at gmail dot com">Richard Opalka</a>
+ */
+public final class BrokenReader extends Reader {
+
+    private static final BrokenReader INSTANCE = new BrokenReader();
+    private final IOException exception;
+
+    private BrokenReader() {
+        exception = new IOException( "Broken reader" );
+    }
+
+    /**
+     * Returns <code>BrokenReader</code> singleton instance.
+     */
+    public static BrokenReader getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public int read( final CharBuffer buffer ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public int read() throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public int read( final char[] buffer ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public int read( final char[] buffer, final int offset, final int length ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void close() throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public long skip( final long count ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public boolean ready() throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void mark( final int readAheadLimit ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void reset() throws IOException {
+        throw exception;
+    }
+}
---
+++ src/main/java/org/fossnova/io/BrokenWriter.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2012, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.fossnova.io;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * A <code>BrokenWriter</code> always throws <code>IOException</code>.
+ * <p>
+ * This class is thread safe. 
+ * </p>
+ * 
+ * @author <a href="mailto:opalka dot richard at gmail dot com">Richard Opalka</a>
+ */
+public final class BrokenWriter extends Writer {
+
+    private static final BrokenWriter INSTANCE = new BrokenWriter();
+    private final IOException exception;
+
+    private BrokenWriter() {
+        exception = new IOException( "Broken writer" );
+    }
+
+    /**
+     * Returns <code>BrokenWriter</code> singleton instance.
+     */
+    public static BrokenWriter getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void write( final int data ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void write( final char[] data ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void write( final char[] data, final int offset, final int length ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void write( final String data ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void write( final String data, final int offset, final int length ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public Writer append( final CharSequence data ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public Writer append( final CharSequence data, final int start, final int end ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public Writer append( final char data ) throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void flush() throws IOException {
+        throw exception;
+    }
+
+    /**
+     * Throws IOException.
+     */
+    @Override
+    public void close() throws IOException {
+        throw exception;
+    }
+}
